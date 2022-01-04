f3943440c33d4294a336b3580bf8a42bc3f571aa
Dilshan <tdm.dev2@gmail.com>
2022-01-04 21:54:19 +0530

    add test for sum.js

---
+++ sum.js
@@ -0,0 +1,11 @@
+//@ts-check
+
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+function sum(a, b) {
+    return a + b;
+}
+
+module.exports = sum;
---
+++ sum.test.js
@@ -0,0 +1,6 @@
+//@ts-check
+const sum = require('./sum.js');
+
+test('add 1 + 2 equal to 3', () => {
+    expect(sum(1, 2)).toBe(3);
+}); 
